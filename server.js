337b8b9f1fe39a36588088cdd29352b14009fc43
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2024-03-25 20:53:41 +0530

    Add console log statement to handle incoming requests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
+  console.log('Request received!');
   res.send('Hello from Express - Version 1!');
 });
 
